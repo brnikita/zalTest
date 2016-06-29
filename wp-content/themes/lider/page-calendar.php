f7570e57e49d691364ff697240851adb72cd8cb8
brnikita <nikita-bragin@mail.ru>
2016-06-29 14:31:37 +0300

    Added section for additional info about schedule. Update2

--- wp-content/themes/lider/page-calendar.php
+++ wp-content/themes/lider/page-calendar.php
@@ -248,22 +248,7 @@
 
     <section class="section_tb info">
         <div class="section_line_lr conteiner">
-            <?php
-            // Start the loop.
-            while (have_posts()) : the_post();
-                if (has_post_thumbnail()) {
-                    $image_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large');
-                    $image_attributes = wp_get_attachment_image_src(get_post_thumbnail_id(), $size, $icon);
-                }
-                ?>
-                <h2>Дополнительная информация о расписании</h2>
-                <?php if ($image_url[0]) { ?>
-                    <img src='<?php echo $image_url[0] ?>' alt="">
-                <?php } ?>
-
-
-                <?php the_content(); ?>
-            <?php endwhile; ?>
+            <?php the_content(); ?>
         </div>
     </section>
 
